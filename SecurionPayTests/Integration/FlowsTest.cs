266d23f804893d68b2c5623ef00387811fae4a89
adamwalocha <adam.walocha@avaus.fi>
2016-04-10 18:30:18 +0200

    test for flow Token -> Charge -> Customer -> Charge (existing card)

--- SecurionPayTests/Integration/FlowsTest.cs
+++ SecurionPayTests/Integration/FlowsTest.cs
@@ -43,12 +43,46 @@ namespace SecurionPayTests.Integration
                 Assert.AreEqual(1, charge.Refunds.Count);
                 Assert.AreEqual(500, charge.Refunds.First().Amount);
                 Assert.IsFalse(charge.Refunds.First().Deleted);
+                Assert.AreEqual(500, charge.Amount);
             }
             catch (SecurionPayException exc)
             {
                 HandleApiException(exc);
             }
 
+        }
+
+
+        /// <summary>
+        /// test for flow Token -> Charge -> Customer -> Charge (existing card)
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task ChargeCustomerFromChargeFlowTest()
+        {
+            try
+            {
+                var createTokenRequest = new TokenRequest() { Number = "4012000100000007", ExpMonth = "11", ExpYear = "2016", Cvc = "432", CardholderName = "Jan Kowalski" };
+                var token = await _gateway.CreateToken(createTokenRequest);
+                token = await _gateway.RetrieveToken(token.Id);
+
+                var chargeRequest = new ChargeRequest() { Amount = 1000, Currency = "PLN", Card = new CardRequest() { Id = token.Id }, Description = "sss" };
+                var charge = await _gateway.CreateCharge(chargeRequest);
+
+                var customerRequest = new CustomerRequest() { Email = "test@test.com", Description = "test customer", Card = new CardRequest() {Id=charge.Id } };
+                var customer = await _gateway.CreateCustomer(customerRequest);
+
+                chargeRequest = new ChargeRequest() { Amount = 1000, Currency = "PLN",CustomerId=customer.Id, Description = "sss"};
+                charge = await _gateway.CreateCharge(chargeRequest);
+
+                Assert.AreEqual(1000, charge.Amount);
+                Assert.AreEqual(customer.Id, charge.CustomerId);
+
+            }
+            catch (SecurionPayException exc)
+            {
+                HandleApiException(exc);
+            }
         }
       
     }
